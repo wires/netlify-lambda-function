a2bb71b3a2b15296ea64b79a8f7046e316fc3bdc
Jelle Herold <jelle@defekt.nl>
2019-03-30 15:32:02 +0100

    working dependencies

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
+let R = require('ramda')
+
 export async function handler(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: `Hello ads ${Math.floor(Math.random() * 10)}` })
+    body: JSON.stringify({ message: `Hello ads ${R.add(2, Math.floor(Math.random() * 10))}` })
   };
 }
